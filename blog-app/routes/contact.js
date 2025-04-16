ceb9fb91b1d2076c299d399695fd5afd38bdab88
Clurixtech <akheituamenkellyrichmond@gmail.com>
2025-04-16 11:37:28 +0100

    added new contact page

--- blog-app/routes/contact.js
+++ blog-app/routes/contact.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const Contact = require('../models/Contact');
 const router = express.Router()
 const contactLayout = "../views/layouts/contact";
 
@@ -9,6 +10,37 @@ router.get('/contact', async (req, res) => {
             description:"A Nodejd Blog App built with Nodejs, Express and MongoDB",
         };
         res.render("contact", {locals, layout: contactLayout})
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.post('/contact', async (req, res) => {
+    try {
+        try {
+            const newContact = new Contact({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                subject: req.body.subject,
+            });
+            await Contact.create(newContact);
+            res.status(201).json({ message: "Sent Successfully", newContact });
+        } catch (error) {
+            console.log(error);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+router.get('/contact/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const contact = await Contact.findById({_id: id});
+        const locals = {
+            title: contact.name,
+            description:"A Nodejd Blog App built with Nodejs, Express and MongoDB",
+        };
+    res.render("request", {locals, contact });
     } catch (error) {
         console.log(error);
     }
